700582c958278b6556a831c234400387a5b20503
Hevedy <Hevedy@users.noreply.github.com>
2019-06-16 18:19:02 +0200

    Array Lerp, Hex grid, Closer, Further, Box tools

--- HEVLibCS/HEVMath.cs
+++ HEVLibCS/HEVMath.cs
@@ -27,6 +27,8 @@ HEVMath.cs
 */
 
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 #if UNITY_EDITOR || UNITY_STANDALONE
 using UnityEngine;
 #else
@@ -150,6 +152,26 @@ namespace HEVLib {
 #else
 			return Math.Clamp( _Value, _Min, _Max );
 #endif
+		}
+
+		public static int Lerp( int _Min, int _Max, float _Time ) {
+#if UNITY_EDITOR || UNITY_STANDALONE
+			return FloorToInt( Mathf.Lerp( _Min, _Max, _Time ) );
+#else
+			return FloorToInt(_Min * ( 1 - _Time ) + _Max * _Time);
+#endif
+		}
+
+		public static float Lerp( float _Min, float _Max, float _Time ) {
+#if UNITY_EDITOR || UNITY_STANDALONE
+			return Mathf.Lerp( _Min, _Max, _Time );
+#else
+			return _Min * ( 1 - _Time ) + _Max * _Time;
+#endif
+		}
+
+		public static double Lerp( double _Min, double _Max, float _Time ) {
+			return _Min * ( 1 - _Time ) + _Max * _Time;
 		}
 
 		public static int FloorToInt( float _Value ) {
@@ -347,31 +369,31 @@ namespace HEVLib {
 			return (a, b);
 		}
 
-		public static sbyte Invert( sbyte _Value ) {
+		public static sbyte OneMinus( sbyte _Value ) {
 			return (sbyte)(_Value * -1);
 		}
 
-		public static short Invert( short _Value ) {
+		public static short OneMinus( short _Value ) {
 			return (short)(_Value * -1);
 		}
 
-		public static int Invert( int _Value ) {
+		public static int OneMinus( int _Value ) {
 			return _Value * -1;
 		}
 
-		public static long Invert( long _Value ) {
+		public static long OneMinus( long _Value ) {
 			return _Value * -1;
 		}
 
-		public static float Invert( float _Value ) {
+		public static float OneMinus( float _Value ) {
 			return _Value * -1.0f;
 		}
 
-		public static double Invert( double _Value ) {
+		public static double OneMinus( double _Value ) {
 			return _Value * -1.0;
 		}
 
-		public static decimal Invert( decimal _Value ) {
+		public static decimal OneMinus( decimal _Value ) {
 			return _Value * -1.0m;
 		}
 
@@ -429,6 +451,142 @@ namespace HEVLib {
 			int m = 1;
 			for ( int i = 0; i <= Count( _Value ); ++i ) { m *= 10; }
 			return b + ( a / m );
+		}
+
+		public static int LerpList( float _Time, List<int> _Array, out float _TimeMax ) {
+			int count = _Array.Count;
+			if ( count < 1 ) { _TimeMax = 0.0f; return 0; }
+			float calTime = (count - 1) * 1.0f;
+			_TimeMax = calTime;
+			float curTime = Min( Max( _Time, 0.0f ), calTime );
+			float curTimeFrac = Fractional( curTime );
+			int curIndex = FloorToInt( curTime );
+			int curIndexVal = _Array[curIndex];
+			int nextIndex = Clamp(curIndex + 1, 0, count - 1);
+			int nextIndexVal = _Array[nextIndex];
+			return Lerp( curIndexVal, nextIndexVal, curTimeFrac );
+		}
+
+		public static float LerpList( float _Time, List<float> _Array, out float _TimeMax ) {
+			int count = _Array.Count;
+			if ( count < 1 ) { _TimeMax = 0.0f; return 0; }
+			float calTime = ( count - 1 ) * 1.0f;
+			_TimeMax = calTime;
+			float curTime = Min( Max( _Time, 0.0f ), calTime );
+			float curTimeFrac = Fractional( curTime );
+			int curIndex = FloorToInt( curTime );
+			float curIndexVal = _Array[curIndex];
+			int nextIndex = Clamp( curIndex + 1, 0, count - 1 );
+			float nextIndexVal = _Array[nextIndex];
+			return Lerp( curIndexVal, nextIndexVal, curTimeFrac );
+		}
+
+		public static float BoxPerimeter( float _XSize, float _YSize ) {
+			return _XSize * 2 + _YSize * 2;
+		}
+
+		public static float BoxDiagonal( float _XSize, float _YSize ) {
+			return (float)Math.Sqrt( ( _XSize * _XSize ) + ( _YSize * _YSize ) );
+		}
+
+		public static float BoxSide( float _Diagonal ) {
+			return (float)Math.Sqrt( ( _Diagonal * _Diagonal ) / 2 );
+		}
+
+		public static float Distance( float _AX, float _AY, float _BX, float _BY ) {
+			return (float)Math.Sqrt( Math.Pow( ( _BX - _AX ), 2 ) + Math.Pow( ( _BY - _AY ), 2 ) );
+		}
+
+		public static int CalculateHexGrid( float _XSize, float _YSize, float _XLocation, float _YLocation, bool _Center, float _Radius, bool _Hex,
+			out int _HexRows, out int _HexColums, out float _HexXSize, out float _HexYSize, out float _HexDistance, out float _HexOverlap, 
+			out List<float> _HexLocationX, out List<float> _HexLocationY ) {
+			float xM = _XSize / 2;
+			float yM = _YSize / 2;
+			float hexSize = _Radius * 2;
+			float hexDistance = BoxSide( hexSize );
+			float hexOverlap = hexSize - hexDistance;
+			int hexXNum = FloorToInt((( _XSize / hexDistance ) + ( _XSize % hexDistance )));
+			int hexYNum = FloorToInt((( _YSize / hexDistance ) + ( _YSize % hexDistance )));
+			float hexXSize = hexXNum * hexDistance;
+			float hexYSize = hexYNum * hexDistance;
+			float fXLocation = _XLocation;
+			float fYLocation = _YLocation;
+			if ( !_Center ) {
+				fXLocation = ( fXLocation - xM );
+				fYLocation = ( fYLocation - yM );
+			} else {
+				fXLocation = fXLocation - ( hexXSize / 2 );
+				fYLocation = fYLocation - ( hexYSize / 2 );
+			}
+
+			float lXDefault = 0;
+			float lYDefault = 0;
+			float lX = lXDefault;
+			float lY = lYDefault;
+
+			List<float> hexLocationX = new List<float>( hexXNum );
+			List<float> hexLocationY = new List<float>( hexYNum );
+
+			bool sw = false;
+			for ( int y = 0; y < hexXNum; y++ ) {
+				if ( sw ) {
+					lX = lXDefault;
+				} else {
+					lX = hexDistance / 2;
+				}
+				for ( int x = 0; x < hexYNum; x++ ) {
+					hexLocationX[x] = lX - (hexDistance / 2);
+					hexLocationY[y] = lY - (hexDistance / 2);
+					lX = lX + hexDistance;
+				}
+				if( _Hex ) sw = !sw;
+				lY = lY + hexDistance;
+			}
+
+			_HexRows = hexXNum;
+			_HexColums = hexYNum;
+			_HexXSize = hexXSize;
+			_HexYSize = hexYSize;
+			_HexDistance = hexDistance;
+			_HexOverlap = hexOverlap;
+
+			_HexLocationX = hexLocationX;
+			_HexLocationY = hexLocationY;
+
+			return (int)(hexXNum * hexYNum);
+		}
+
+		public static int SimulateHexGrid( float _GridXSize, float _GridYSize, float _GridXLocation, float _GridYLocation, bool _GridCenter, float _HexRadius,
+			float _CurXLocation, float _CurYLocation, out float _HexXLocation, out float _HexYLocation, out float _Distance, bool _Hex = true ) {
+
+			int hexRows = 0;
+			int hexColums = 0;
+			float hexXSize = 0;
+			float hexYSize = 0;
+			float hexDistance = 0;
+			float hexOverlap = 0;
+			List<float> hexLocationX; 
+			List<float> hexLocationY;
+
+			int hexNum = CalculateHexGrid( _GridXSize, _GridYSize, _GridXLocation, _GridYLocation, _GridCenter, _HexRadius, _Hex,
+			out hexRows, out hexColums, out hexXSize, out hexYSize, out hexDistance, out hexOverlap, out hexLocationX, out hexLocationY );
+
+			float distance = Distance( hexLocationX[0], hexLocationY[0], _CurXLocation, _CurYLocation );
+			int index = 0;
+			float distCalc;
+			for ( int i = 0; i < hexLocationX.Count; i++ ) {
+				distCalc = Distance( hexLocationX[i], hexLocationY[i], _CurXLocation, _CurYLocation );
+				if( distCalc < distance ) {
+					distance = distCalc;
+					index = i;
+				}
+			}
+
+			_HexXLocation = hexLocationX[index];
+			_HexYLocation = hexLocationY[index];
+			_Distance = distance;
+
+			return index;
 		}
 
 		public static sbyte Closer( sbyte _Value, sbyte _A, sbyte _B ) {
@@ -500,6 +658,69 @@ namespace HEVLib {
 			return ( ( ( _Value - _A ) >= 0 ) ? ( _Value - _A ) : -( _Value - _A ) ) >= ( ( ( _Value - _B ) >= 0 ) ? 
 				( _Value - _B ) : -( _Value - _B ) ) ? _A : _B;
 		}
+
+
+		/**
+		* Closer number to given of Array
+		* @param	Array of templated type
+		* @param	Reference value templated type to compare
+		* @param	Optional boolean to search only the closer values and no the equals
+		* @param	Optional pointer for returning the index of the closer element, if multiple closer elements the first index is returned
+		* @return	The closer value found in the array or default value if the array was empty or can't find a correct value
+		*/
+		public static float Closer( float _Reference, List<float> _Values, bool _NotEqual, out int _CloserIndex ) {
+			if ( _Values.Count == 0 ) {
+				_CloserIndex = 0;
+				return 0f;
+			}
+
+			float curCloser = ( ( _Reference - _Values[0] ) >= 0 ) ? ( _Reference - _Values[0] ) : -( _Reference - _Values[0] );
+			int curCloserIndex = 0;
+			if ( curCloser == 0 && _NotEqual ) {
+				curCloserIndex = -1;
+			}
+
+			for ( int v = 1; v < _Values.Count; ++v ) {
+				float value = ( ( _Reference - _Values[v] ) >= 0 ) ? ( _Reference - _Values[v] ) : -( _Reference - _Values[v] );
+				if ( value < curCloser ) {
+					if ( !_NotEqual || ( _NotEqual && value != 0f ) ) {
+						curCloser = value;
+						curCloserIndex = v;
+					}
+				}
+			}
+
+			_CloserIndex = curCloserIndex == -1 ? 0 : curCloserIndex;
+			return _Values.Count == 0 ? 0 : _Values[curCloserIndex == -1 ? 0 : curCloserIndex];
+		}
+
+		/**
+		* Further number to given of Array
+		* @param	Array of templated type
+		* @param	Reference value templated type to compare
+		* @param	Optional pointer for returning the index of the further element, if multiple further elements the first index is returned
+		* @return	The further value found in the array or default value if the array was empty
+		*/
+		public static float Further( float _Reference, List<float> _Values, out int _FurtherIndex ) {
+			if ( _Values.Count == 0 ) {
+				_FurtherIndex = 0;
+				return 0f;
+			}
+
+			float curFurther = ( ( _Reference - _Values[0] ) >= 0 ) ? ( _Reference - _Values[0] ) : -( _Reference - _Values[0] );
+			int curFurtherIndex = 0;
+			for ( int v = 1; v < _Values.Count; ++v ) {
+				float value = ( ( _Reference - _Values[v] ) >= 0 ) ? ( _Reference - _Values[v] ) : -( _Reference - _Values[v] );
+				if ( curFurther < value ) {
+					curFurther = value;
+					curFurtherIndex = v;
+				}
+			}
+
+			_FurtherIndex = curFurtherIndex == -1 ? 0 : curFurtherIndex;
+			return _Values.Count == 0 ? 0 : _Values[curFurtherIndex == -1 ? 0 : curFurtherIndex];
+		}
+
 	}
 
 
