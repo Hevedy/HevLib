8ad0fc4d141df91ed00dee73746d514c1f879013
Hevedy <Hevedy@users.noreply.github.com>
2019-07-25 22:17:02 +0200

    Create HEVLib.cpp

---
+++ UnrealEngine/Plugins/HEVLib/Source/Private/HEVLib.cpp
@@ -0,0 +1,48 @@
+/*
+===========================================================================
+This code is part of the HevLib source code content in
+HevLib by Hevedy <https://github.com/Hevedy/HevLib>
+Mozilla Public License Version 2.0 (MPL-2.0)
+Copyright (c) 2018-2019 Hevedy <https://github.com/Hevedy>
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+===========================================================================
+*/
+
+/*
+================================================
+HEVLib.cpp
+================================================
+*/
+
+
+#include "IHEVLib.h"
+#include "HEVLibPrivatePCH.h"
+
+class FHEVLib : public IHEVLib {
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+IMPLEMENT_MODULE( FHEVLib, HEVLib )
+
+
+
+void FHEVLib::StartupModule() {
+	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
+}
+
+
+void FHEVLib::ShutdownModule() {
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
